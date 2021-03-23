536fd0dd463f3c9087f77645dd253659c2d23ec7
Ryan Jennings <ryanjennings1010@gmail.com>
2021-03-23 17:37:20 +0000

    Initial commit

---
+++ src/mapsql/sql/test/ExampleTestProgram.java
@@ -0,0 +1,182 @@
+package mapsql.sql.test;
+
+import mapsql.sql.condition.Equals;
+import mapsql.sql.condition.LessThan;
+import mapsql.sql.condition.OrCondition;
+import mapsql.sql.core.Condition;
+import mapsql.sql.core.Field;
+import mapsql.sql.core.SQLException;
+import mapsql.sql.core.SQLManager;
+import mapsql.sql.core.SQLResult;
+import mapsql.sql.core.SQLStatement;
+import mapsql.sql.field.CHARACTER;
+import mapsql.sql.field.INTEGER;
+import mapsql.sql.statement.CreateTable;
+import mapsql.sql.statement.Delete;
+import mapsql.sql.statement.DropTable;
+import mapsql.sql.statement.Insert;
+import mapsql.sql.statement.Select;
+import mapsql.sql.statement.Update;
+
+public class ExampleTestProgram {
+	static SQLManager manager = new SQLManager();
+	
+	public static void main(String[] args) {
+		createTableStatement();
+
+		showTables();
+		insertData();
+		selectTable();
+
+		insertPartialData();
+		selectTable();
+		
+//		updateData();
+		updateOrData();
+		selectTable();
+
+		deleteData();
+		selectTable();
+		
+		dropTable();
+		showTables();
+
+		testLessThan();
+	}
+
+	private static void executeStatement(SQLStatement statement) {
+		try {
+			SQLResult result = manager.execute(statement);
+			System.out.println(result);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void createTableStatement() {
+		executeStatement(
+				new CreateTable(
+						"contact", 
+						new Field[] {
+								new INTEGER("id", true, false, true), 
+								new CHARACTER("name", 30, false, true), 
+								new CHARACTER("email", 30, false, false)
+						}
+				)
+		);
+	}
+	
+	public static void showTables() {
+		executeStatement(new Select("mapsql.tables", new String[] { "*" }));
+	}
+	
+	public static void selectTable() {
+		executeStatement(new Select("contact", new String[] { "*" }));
+	}
+	
+	public static void dropTable() {
+		executeStatement(new DropTable("contact"));
+	}
+	
+	public static void insertData() {
+		executeStatement(
+				new Insert(
+						"contact", 
+						new String[] {"name", "email"}, 
+						new String[] {"Rem", "rem.collier@ucd.ie"}
+				)
+		);
+	}
+
+	public static void insertPartialData() {
+		executeStatement(new Insert("contact", new String[] {"name"}, new String[] {"Henry"}));
+	}
+
+	public static void updateData() {
+		executeStatement(
+				new Update(
+						"contact", 
+						new String[] {"email"}, 
+						new String[] {"henry.mcloughlin@ucd.ie"}, 
+						new Equals("id", "2")
+				)
+		);
+	}
+	
+	public static void updateOrData() {
+		executeStatement(
+				new Update(
+						"contact", 
+						new String[] {"email"}, 
+						new String[] {"henry.mcloughlin@ucd.ie"}, 
+						new OrCondition(
+								new Equals("id", "1"), 
+								new Equals("id", "2")
+						)
+				)
+		);
+	}
+	
+	public static void deleteData() {
+		executeStatement(new Delete("contact", new Equals("id", "2")));
+	}
+	
+	public static void selectTableWithColumns() {
+		executeStatement(new Select("contact", new String[] { "id", "name" }));
+	}
+
+	public static void testLessThan() {
+		// Create new table
+		executeStatement(new CreateTable(
+							"contacts", 
+							new Field[] {
+								new INTEGER("id", true, false, true), 
+								new CHARACTER("name", 30, false, true), 
+								new CHARACTER("email", 30, false, false)
+							}
+						)
+		);
+		// Add in two contacts
+		executeStatement(
+				new Insert(
+						"contacts", 
+						new String[] {"name", "email"}, 
+						new String[] {"Rem", "rem.collier@ucd.ie"}
+				)
+		);
+		executeStatement(
+				new Insert(
+						"contacts", 
+						new String[] {"name", "email"}, 
+						new String[] {"John", "john.murphy@ucd.ie"}
+				)
+		);
+		// Check select returns 1 of 2 contacts
+		try {
+			SQLResult result = manager.execute(
+					new Select(
+						"contacts",
+						new String[] { "id", "name" },
+						new LessThan("id", "2")
+					)
+			);
+			System.out.println(result);
+			assertResult("testLessThan: Expected: %b, Actual: %b\n", true, result.rows().size() == 1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// Drop table
+		executeStatement(new DropTable("contacts"));
+	}
+
+	private static void assertResult(String printfMessage, Boolean expected, Boolean actual) {
+		if (expected == actual) {
+			System.out.printf(printfMessage, expected, actual);
+		} else {
+			System.err.printf(printfMessage, expected, actual);
+		}
+		
+	}
+}
