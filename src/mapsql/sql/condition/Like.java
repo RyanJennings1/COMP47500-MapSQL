ba0b5d77e1aa7dd90c849eb7136ae21f4e30c3d2
Ryan Jennings <ryanjennings1010@gmail.com>
2021-03-25 17:43:19 +0000

    Added comments and checks to LIKE

--- src/mapsql/sql/condition/Like.java
+++ src/mapsql/sql/condition/Like.java
@@ -20,11 +20,28 @@ public class Like extends AbstractCondition {
 	@Override
 	public boolean evaluate(TableDescription description, Map<String, String> data) throws SQLException {
 		String query = data.get(column);
+		/**
+		 * If the query begins with % but doesn't end with it then it is in
+		 * the form '%....'
+		 * If the query ends with % but doesn't start with it then it is in the 
+		 * form '....%'
+		 * Otherwise if it contains % but failed the first two statements then it
+		 * has to be in the form %...%
+		 */
+		int count_per = 0;
+		for (char ch: value.toCharArray()) {
+			if (ch == '%') {
+				count_per++;
+			}
+			if (count_per > 2) {
+				throw new SQLException("Incorrect use of LIKE operator");
+			}
+		}
 		if (value.startsWith("%") && !value.endsWith("%")) {
 			return query.endsWith(value.substring(1));
 		} else if (value.endsWith("%") && !value.startsWith("%")) {
 			return query.startsWith(value.substring(0, value.length()-1));
-		} else if (value.contains("%")){
+		} else if (value.startsWith("%") && value.endsWith("%")){
 			return query.contains(value.split("%")[1]);
 		}
 		return false;
